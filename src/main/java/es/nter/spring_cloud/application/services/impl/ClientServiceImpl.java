11800f9af3851152132891853787dfbc027cec6b
memecito <memecito@users.noreply.github.com>
2025-10-17 13:07:14 +0200

    creacion de los servicios

--- src/main/java/es/nter/spring_cloud/application/services/impl/ClientServiceImpl.java
+++ src/main/java/es/nter/spring_cloud/application/services/impl/ClientServiceImpl.java
@@ -1,36 +1,55 @@
 package es.nter.spring_cloud.application.services.impl;
 
+import es.nter.spring_cloud.application.mappers.ClientMapper;
 import es.nter.spring_cloud.application.services.ClientService;
+import es.nter.spring_cloud.exception.EntityNotFoundException;
 import es.nter.spring_cloud.persistence.entities.Client;
+import es.nter.spring_cloud.persistence.repositories.ClientRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Service
 @RequiredArgsConstructor
 public class ClientServiceImpl implements ClientService {
+
+    private final ClientRepository repository;
+    private final ClientMapper mapper;
     @Override
     public Page<Client> getAll(int page, int size) {
-        return null;
+        Pageable pageable= PageRequest.of(page, size);
+
+        return repository.findAll(pageable);
     }
 
     @Override
     public Client getById(Long id) {
-        return null;
+        return repository.findById(id).orElseThrow(
+                ()->new EntityNotFoundException("Cliente not found")
+        );
     }
 
     @Override
+    @Transactional
     public Client created(Client client) {
-        return null;
+        return repository.save(client);
     }
 
     @Override
+    @Transactional
     public Client updated(Long id, Client client) {
-        return null;
+        Client clientFound= getById(id);
+        return mapper.toUpdate(clientFound,client);
     }
 
     @Override
+    @Transactional
     public void deleted(Long id) {
+        repository.delete(getById(id));
 
     }
 }
--- src/main/java/es/nter/spring_cloud/application/services/impl/TripServiceImpl.java
+++ src/main/java/es/nter/spring_cloud/application/services/impl/TripServiceImpl.java
@@ -1,36 +1,52 @@
 package es.nter.spring_cloud.application.services.impl;
 
+import es.nter.spring_cloud.application.mappers.TripMapper;
 import es.nter.spring_cloud.application.services.TripService;
+import es.nter.spring_cloud.exception.EntityNotFoundException;
 import es.nter.spring_cloud.persistence.entities.Trip;
+import es.nter.spring_cloud.persistence.repositories.TripRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TripServiceImpl implements TripService {
+
+    private final TripRepository repository;
+    private final TripMapper mapper;
     @Override
     public Page<Trip> getAll(int page, int size) {
-        return null;
+        Pageable pageable= PageRequest.of(page, size);
+        return repository.findAll(pageable);
     }
 
     @Override
     public Trip getById(Long id) {
-        return null;
+        return repository.findById(id).orElseThrow(
+                ()->new EntityNotFoundException("Cliente no encontrado")
+        );
     }
 
     @Override
+    @Transactional
     public Trip created(Trip trip) {
-        return null;
+        return repository.save(trip);
     }
 
     @Override
+    @Transactional
     public Trip updated(Long id, Trip trip) {
-        return null;
+        Trip tripFound= getById(id);
+        return mapper.toUpdate(tripFound, trip);
     }
 
     @Override
+    @Transactional
     public void deleted(Long id) {
-
+        repository.delete(getById(id));
     }
 }
